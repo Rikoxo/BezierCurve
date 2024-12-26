46e63b4294c589d6d69f7f3a6e27b478af4ab6b5
Rikoxo <90527932+Rikoxo@users.noreply.github.com>
2024-12-26 11:27:17 +0100

    Update exemple.jl

--- exemple.jl
+++ exemple.jl
@@ -1,10 +1,6 @@
-using Plots
 using BezierCurve
 
-
 function main()
-      # Votre module
-
     fig = plot([], [], label="Bezier Curves", size=(600, 600), xlims=(-5, 5), ylims=(-5, 5))
 
     xp1 = (-4, -2, 0, 2, 4)
@@ -13,16 +9,16 @@ function main()
     xp2 = (-4, -1, 1, 3, 4)
     yp2 = (1, 2, 3, -1, 0)
 
-    tx1, ty1 = BezierPlot(xp1, yp1)
-    tx2, ty2 = BezierPlot(xp2, yp2)
+    # Convertir les tuples en vecteurs
+    tx1, ty1 = BezierPlot(collect(xp1), collect(yp1))
+    tx2, ty2 = BezierPlot(collect(xp2), collect(yp2))
 
     plot!(fig, tx1, ty1, label="Bezier Curve 1", color="blue")
     plot!(fig, tx2, ty2, label="Bezier Curve 2", color="red")
 
-    scatter!(fig, xp1, yp1, label="Control Points Curve 1", color="blue", marker=:circle)
-    scatter!(fig, xp2, yp2, label="Control Points Curve 2", color="red", marker=:circle)
+    scatter!(fig, collect(xp1), collect(yp1), label="Control Points Curve 1", color="blue", marker=:circle)
+    scatter!(fig, collect(xp2), collect(yp2), label="Control Points Curve 2", color="red", marker=:circle)
 
-    # Convertir les tuples en vecteurs pour FindIntersection
     intersections = FindIntersection(collect(xp1), collect(yp1), collect(xp2), collect(yp2))
 
     for (x, y) in intersections
